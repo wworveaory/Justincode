531d98693cb6790fbeb0d1ab2465cd37782a48bd
wworveaory <wworveaory@gmail.com>
2021-08-12 16:07:04 +0800

    CDS
    CD

--- my-work/js-work/public/index.js
+++ my-work/js-work/public/index.js
@@ -1,47 +1,70 @@
-const aa = document.getElementById('aa')
-let a = [{
-        name: '黃家承',
-        age: 41,
-        gender: 'girl',
-        hairColor: 'pink',
-        abc: 2312,
-    },
-    {
-        name: '成名威',
-        age: 46,
-        gender: 'gisrl',
-        hairColor: 'piddenk',
-        abc: 2312,
-    },
-    {
-        name: '吳俊碩',
-        age: 1,
-        gender: 'de因為愛l',
-        hairColor: 'eepink',
-        abc: 2312,
-    },
-    {
-        name: '桑若紅',
-        age: 66,
-        gender: 'd你知道rl',
-        hairColor: 'ee嗎nk',
-        abc: 2312,
-    },
-    {
-        name: '乖孫',
-        age: 18,
-        gender: 'aa道rl',
-        hairColor: '妳好k',
-        abc: 59595,
-    }
-]
-let name = ""
+//把js直轉成json字符串
+// const aObj = { a: '你好', b: '我好', c :123 }
+// let aJson = JSON.stringify(aObj)
+// console.log(aJson);
 
-// aa.innerHTML=`我的名子叫做${b}`
-for (let i = 0; i < a.length; i++) {
-	console.log(i);
 
-    name +=`我的名子叫做${a[i].name}，今年${a[i].age}歲!!`
-  
-}
-aa.innerHTML = `<h1>${name}</h1>`
+
+//把json格式轉成字串
+// const ajson='{"a":"dfe","c":"要確定"}'
+// let k=JSON.parse(ajson)
+// console.log(k);
+// const jsonB = '{"ee":"帥哥","a":"醜歌","d":"太帥哥"}'
+// let c = JSON.parse(jsonB)
+// console.log(c)
+
+// 混合格式
+//先把他轉成json 在再轉成物件
+// let remix ={a="我好喔", b="你好喔"}
+
+// let remixB=JSON.parse(JSON.stringify(remix))
+// console.log(remixB);
+
+// ------------------------------------------------------
+//淺拷貝
+// let remix ={a:"我好喔", b:"你好喔",c:"我好喔",d:"他好喔"}
+// let y=Object.assign({},remix)
+// console.log(y);
+
+
+// -----物件轉陣列-=============
+// let aObject={a:'想黃',b:'小白',c:'想綠',d:'小乖'}
+// //將物件的key值轉成陣列
+// let aArray=Object.keys(aObject)
+// console.log(aArray);
+// //將陣列轉字串
+// let d =aArray.join('')
+// console.log(d);
+// --------------------------------------------------
+// let aObject={a:'想黃',b:'小白',c:'想綠',d:'小乖'}
+// ///淺拷貝
+// let i =Object.assign({},aObject)
+// console.log(i);
+// ///淺拷貝
+// let cc={...aObject}
+// console.log(cc);
+//物件轉json格式
+// let b=JSON.stringify(aObject)
+// console.log(b);
+// --------------------------------------------
+//json 轉物件
+
+// let aJson='{"a":"乖","b":"孫","c":"在","d":"嗎"}'
+// let aObject=JSON.parse(aJson)
+// console.log(aObject);
+// -------------------------------------------------
+const aa=document.getElementById('aa')
+let aJson='{"a":"乖","b":"孫","c":"在","d":"嗎"}'
+//json轉物件
+let aObject=JSON.parse(aJson)
+console.log(aObject);
+//再取key值 
+let array=Object.keys(aObject)
+console.log(array);
+// 先將物件變成陣列
+let str=""
+array.forEach(function(v){
+    str+=`${v}`
+})
+console.log(str);
+aa.innerHTML=str
